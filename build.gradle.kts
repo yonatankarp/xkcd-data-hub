55b66e473ce11879bae59d8382df8220c23640c3
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-06-03 18:58:43 +0200

    Add mutation testing (Closes #13) (#14)
    This commit adds PIT as the mutation test framework for all subprojects within this repository.
    
    Later on, the report will be calculated once a week by a GitHub cronjob and updated in the UI. However, for now the report can be called only manually as the calculation time of it takes some time.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,32 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktor) apply false
+    alias(libs.plugins.pitest) apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
     group = "com.xkcddatahub"
     version = "0.0.1"
 
-    repositories {
-        mavenCentral()
+    apply(plugin = "info.solidsoft.pitest")
+
+    afterEvaluate {
+        plugins.withId("info.solidsoft.pitest") {
+            configure<info.solidsoft.gradle.pitest.PitestPluginExtension> {
+                targetClasses.set(listOf("com.xkcddatahub.*"))
+                threads.set(Runtime.getRuntime().availableProcessors())
+                outputFormats.set(listOf("XML", "HTML"))
+                timestampedReports.set(false)
+                excludedClasses = listOf(
+                    "com.xkcddatahub.*.di.*"
+                )
+            }
+        }
     }
 }
