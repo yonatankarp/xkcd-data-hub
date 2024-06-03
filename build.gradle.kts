577db69bab323f20cc3c106acc6529a24572b973
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-06-03 21:35:09 +0200

    Integrate spotless
    This commit adds spotless as the linter tool for Kotlin, Gradle and Markdown in the project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktor) apply false
     alias(libs.plugins.pitest) apply false
+    alias(libs.plugins.spotless) apply true
 }
 
 allprojects {
@@ -16,8 +19,26 @@ subprojects {
     version = "0.0.1"
 
     apply(plugin = "info.solidsoft.pitest")
+    apply(plugin = "com.diffplug.spotless")
 
     afterEvaluate {
+        plugins.withId("com.diffplug.spotless") {
+            configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+                kotlin {
+                    ktlint()
+                    target("src/**/*.kt")
+                }
+                kotlinGradle {
+                    ktlint()
+                    target("src/**/*.kts")
+                }
+                flexmark {
+                    flexmark()
+                    target("src/**/*.md")
+                }
+            }
+        }
+
         plugins.withId("info.solidsoft.pitest") {
             configure<info.solidsoft.gradle.pitest.PitestPluginExtension> {
                 targetClasses.set(listOf("com.xkcddatahub.*"))
@@ -33,6 +54,10 @@ subprojects {
 
         tasks.withType(Test::class) {
             useJUnitPlatform()
+        }
+
+        tasks.withType(KotlinCompile::class.java) {
+            dependsOn("spotlessCheck")
         }
     }
 }
