85e24ed2576e0a51bf7e4d72b2b69afb49446414
Yonatan Karp-Rudin <yonvata@gmail.com>
2025-08-12 08:42:29 +0200

    Update Koin to 4.1.0

--- fetcher/src/test/kotlin/com/xkcddatahub/fetcher/bootstrap/di/DependencyInjectionTest.kt
+++ fetcher/src/test/kotlin/com/xkcddatahub/fetcher/bootstrap/di/DependencyInjectionTest.kt
@@ -1,18 +1,35 @@
 package com.xkcddatahub.fetcher.bootstrap.di
 
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpStatusCode
 import org.junit.jupiter.api.Test
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.module
 import org.koin.test.KoinTest
-import org.koin.test.check.checkModules
+import org.koin.test.verify.Verify.verify
 
+@OptIn(KoinExperimentalAPI::class)
 class DependencyInjectionTest : KoinTest {
     @Test
     fun `dependency injection context is loaded correctly`() {
-        checkModules {
-            modules(
-                networkModule,
-                adapterModule(xkcdBaseUrl = "https://xkcd.com"),
-                appModule,
-            )
-        }
+        val allModules =
+            module {
+                includes(
+                    networkModule,
+                    adapterModule(xkcdBaseUrl = "https://xkcd.com"),
+                    appModule,
+                )
+            }
+
+        val testModule =
+            module {
+                single<HttpClientEngine> {
+                    MockEngine { respond("ok", HttpStatusCode.OK) }
+                }
+            }
+
+        verify(module = module { includes(allModules, testModule) })
     }
 }
